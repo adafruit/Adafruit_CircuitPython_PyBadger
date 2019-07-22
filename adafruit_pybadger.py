78746dab39ca76dd4328b86397a587f9e3a7b568
Melissa LeBlanc-Williams <melissa@adafruit.com>
2019-07-22 16:38:41 -0700

    Un-nest the try/except blocks and move try/except under if statement

--- adafruit_pybadger.py
+++ adafruit_pybadger.py
@@ -83,16 +83,18 @@ class PyBadger:
 
     def __init__(self, i2c=None):
         # Accelerometer
-        try:
-            if i2c is None:
+        if i2c is None:
+            try:
                 i2c = board.I2C()
+            except RuntimeError:
+                self._accelerometer = None
+
+        if i2c is not None:
             int1 = digitalio.DigitalInOut(board.ACCELEROMETER_INTERRUPT)
             try:
                 self._accelerometer = adafruit_lis3dh.LIS3DH_I2C(i2c, address=0x19, int1=int1)
             except ValueError:
                 self._accelerometer = adafruit_lis3dh.LIS3DH_I2C(i2c, int1=int1)
-        except RuntimeError:
-            self._accelerometer = None
 
         # Buttons
         self._buttons = GamePadShift(digitalio.DigitalInOut(board.BUTTON_CLOCK),

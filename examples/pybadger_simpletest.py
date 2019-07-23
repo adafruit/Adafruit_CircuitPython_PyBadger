45fb2849e37902c089d58b110612b324498da85b
Melissa LeBlanc-Williams <melissa@adafruit.com>
2019-07-23 08:25:17 -0700

    Linting

--- examples/pybadger_simpletest.py
+++ examples/pybadger_simpletest.py
@@ -5,7 +5,7 @@ pybadger = PyBadger()
 pybadger.show_badge(name_string="Blinka", hello_scale=2, my_name_is_scale=2, name_scale=3)
 
 while True:
-    pybadger.auto_dim_display(delay=10) # Remove this line if you have the PyBadge LC since there's no accelerometer
+    pybadger.auto_dim_display(delay=10) # Remove or comment out this line if you have the PyBadge LC
     if pybadger.button.a:
         pybadger.show_business_card(image_name="Blinka.bmp", name_string="Blinka", name_scale=2,
                                     email_string_one="blinka@", email_string_two="adafruit.com")

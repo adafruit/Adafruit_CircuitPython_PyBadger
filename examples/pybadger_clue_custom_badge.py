ec3e5236ae170ace059009f2296104d95318aa2c
Kattni Rembor <kattni@adafruit.com>
2020-02-27 11:49:14 -0500

    Rename show to show_custom_badge

--- examples/pybadger_clue_custom_badge.py
+++ examples/pybadger_clue_custom_badge.py
@@ -9,11 +9,11 @@ pybadger.badge_line(text="Blinka", color=pybadger.WHITE, scale=5, padding_above=
 pybadger.badge_line(text="CircuitPythonista", color=pybadger.WHITE, scale=2, padding_above=2)
 pybadger.badge_line(text="she/her", color=pybadger.BLINKA_PINK, scale=4, padding_above=4)
 
-pybadger.show()
+pybadger.show_custom_badge()
 
 while True:
     if pybadger.button.a:
-        pybadger.show()
+        pybadger.show_qr_code()
 
     if pybadger.button.b:
-        pybadger.show_qr_code()
+        pybadger.show_custom_badge()
